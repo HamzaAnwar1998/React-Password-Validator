aa3d8b7f1f6e70e9f4a4a7c58c2877b1f4a3e8b7
Hamza <anwarhamza919@gmail.com>
2022-10-03 17:03:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-useless-escape */
+import {useState} from 'react';
+import Icon from 'react-icons-kit';
+import {basic_eye} from 'react-icons-kit/linea/basic_eye'
+import {basic_eye_closed} from 'react-icons-kit/linea/basic_eye_closed'
+import {arrows_exclamation} from 'react-icons-kit/linea/arrows_exclamation'
+import {arrows_circle_check} from 'react-icons-kit/linea/arrows_circle_check'
 
 function App() {
+
+  const [type, setType] = useState('password');
+
+  // validated states
+  const [lowerValidated, setLowerValidated]=useState(false);
+  const [upperValidated, setUpperValidated]=useState(false);
+  const [numberValidated, setNumberValidated]=useState(false);
+  const [specialValidated, setSpecialValidated]=useState(false);
+  const [lengthValidated, setLengthValidated]=useState(false);
+
+  const handleChange=(value)=>{
+    const lower = new RegExp('(?=.*[a-z])');
+    const upper = new RegExp('(?=.*[A-Z])');
+    const number = new RegExp('(?=.*[0-9])');
+    const special = new RegExp('(?=.*[!@#\$%\^&\*])');
+    const length = new RegExp('(?=.{8,})')
+    if(lower.test(value)){
+      setLowerValidated(true);
+    }
+    else{
+      setLowerValidated(false);
+    }
+    if(upper.test(value)){
+      setUpperValidated(true);
+    }
+    else{
+      setUpperValidated(false);
+    }
+    if(number.test(value)){
+      setNumberValidated(true);
+    }
+    else{
+      setNumberValidated(false);
+    }
+    if(special.test(value)){
+      setSpecialValidated(true);
+    }
+    else{
+      setSpecialValidated(false);
+    }
+    if(length.test(value)){
+      setLengthValidated(true);
+    }
+    else{
+      setLengthValidated(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+
+      {/* box containing input field and validation tracker  */}
+      <div className="box">
+
+        {/* input */}
+        <label>Enter Password</label>
+        <div className='input-with-icon-div form-control'>
+          <input className='custom-input' type={type}
+          onChange={(e)=>handleChange(e.target.value)}/>
+          {type==="password"?(
+            <span className='icon-span' onClick={()=>setType("text")}>
+              <Icon icon={basic_eye_closed} size={18}/>
+            </span>
+          ):(
+            <span className='icon-span' onClick={()=>setType("password")}>
+              <Icon icon={basic_eye} size={18}/>
+            </span>
+          )}
+        </div>
+
+        {/* validation tracker */}
+        <main className='tracker-box'>
+          <div className={lowerValidated?'validated':'not-validated'}>
+            {lowerValidated?(
+              <span className='list-icon green'>
+                <Icon icon={arrows_circle_check}/>  
+              </span>
+            ):(
+              <span className='list-icon'>
+                <Icon icon={arrows_exclamation}/>  
+              </span>
+            )}
+            At least one lowercase letter
+          </div>
+          <div className={upperValidated?'validated':'not-validated'}>
+            {upperValidated?(
+              <span className='list-icon green'>
+                <Icon icon={arrows_circle_check}/>  
+              </span>
+            ):(
+              <span className='list-icon'>
+                <Icon icon={arrows_exclamation}/>  
+              </span>
+            )}
+            At least one uppercase letter
+          </div>
+          <div className={numberValidated?'validated':'not-validated'}>
+            {numberValidated?(
+              <span className='list-icon green'>
+                <Icon icon={arrows_circle_check}/>  
+              </span>
+            ):(
+              <span className='list-icon'>
+                <Icon icon={arrows_exclamation}/>  
+              </span>
+            )}
+            At least one number
+          </div>
+          <div className={specialValidated?'validated':'not-validated'}>
+            {specialValidated?(
+              <span className='list-icon green'>
+                <Icon icon={arrows_circle_check}/>  
+              </span>
+            ):(
+              <span className='list-icon'>
+                <Icon icon={arrows_exclamation}/>  
+              </span>
+            )}
+            At least one special character
+          </div>
+          <div className={lengthValidated?'validated':'not-validated'}>
+            {lengthValidated?(
+              <span className='list-icon green'>
+                <Icon icon={arrows_circle_check}/>  
+              </span>
+            ):(
+              <span className='list-icon'>
+                <Icon icon={arrows_exclamation}/>  
+              </span>
+            )}
+            At least 8 characters
+          </div>
+        </main>
+      </div>
+
     </div>
   );
 }
